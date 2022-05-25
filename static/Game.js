ff9adb55a3aaa3ff4d62ca1f96f323aade5bb1c6
Kuba <kubakowal2004@gmail.com>
2022-05-25 21:55:18 +0200

    generate table

--- static/Game.js
+++ static/Game.js
@@ -5,7 +5,8 @@ class Game {
         this.clickedField = "none";
         this.fieldSize = 22;
         this.pawnRadius = 8;
-        this.szachownica = [
+        // white - 0 | black - 2
+        this.checkboard = [
             [1, 0, 1, 0, 1, 0, 1, 0],
             [0, 1, 0, 1, 0, 1, 0, 1],
             [1, 0, 1, 0, 1, 0, 1, 0],
@@ -16,8 +17,8 @@ class Game {
             [0, 1, 0, 1, 0, 1, 0, 1]
         ];
 
-        // białe - 1 | czarne - 2 | puste, bez pionka - 0
-        this.pionki = [
+        // white - 1 | black - 2 | empty field - 0
+        this.pawns = [
             [1, 0, 1, 0, 1, 0, 1, 0],
             [0, 1, 0, 1, 0, 1, 0, 1],
             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -34,19 +35,76 @@ class Game {
         this.renderer.setClearColor(0x333333);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.getElementById("root").append(this.renderer.domElement);
-        this.camera.position.set(0, 300, 0)
+        this.camera.position.set(0, 200, 0)
         this.camera.lookAt(this.scene.position);
 
         this.axes = new THREE.AxesHelper(1000)
         window.addEventListener('resize', this.onWindowResize, false);
         this.scene.add(this.axes);
+        
+        this.generateTable(450, 30, 10); // length, width, height
         this.render()
+    }
+
+    generateTable = (length, width, height) => {
+        this.table = new THREE.Object3D();
+
+        const insideGeometry = new THREE.BoxGeometry( length-width, height-1, length-width );
+        const insideMaterial = new THREE.MeshBasicMaterial( {
+            side: THREE.DoubleSide,
+            map: new THREE.TextureLoader().load('materials/table.jpg', function (texture) {
+                texture.wrapS = THREE.RepeatWrapping;
+                texture.wrapT = THREE.RepeatWrapping;
+                texture.repeat.x = length / 50;
+                texture.repeat.y = length / 50;
+            })
+        } );
+
+        const tableInside = new THREE.Mesh( insideGeometry, insideMaterial );
+        tableInside.position.set(0, 0, 0);
+        
+        this.table.add(tableInside)
+
+        const tableOutside = new THREE.Object3D();
+
+        const outsideGeometry = new THREE.BoxGeometry( length, height, width );
+        const outsideMaterial = new THREE.MeshBasicMaterial( {
+            side: THREE.DoubleSide,
+            map: new THREE.TextureLoader().load('materials/tableBorder.jpg', function (texture) {
+                texture.wrapS = THREE.RepeatWrapping;
+                texture.wrapT = THREE.RepeatWrapping;
+                texture.repeat.x = length / 40;
+                texture.repeat.y = width / 40;
+            }),
+        } );
+
+        let firstSide = new THREE.Mesh( outsideGeometry, outsideMaterial );
+        firstSide.position.set(0, 0, (-length/2) + (width/2));
+        tableOutside.add( firstSide );
+
+        let secondSide = new THREE.Mesh( outsideGeometry, outsideMaterial );
+        secondSide.position.set((-length/2) + (width/2), 0, 0)
+        secondSide.rotation.y = Math.PI / 2;
+        tableOutside.add( secondSide );
+
+        let thirdSide = new THREE.Mesh( outsideGeometry, outsideMaterial );
+        thirdSide.position.set(0, 0, (length/2) - (width/2))
+        tableOutside.add( thirdSide );
+
+        let fourthSide = new THREE.Mesh( outsideGeometry, outsideMaterial );
+        fourthSide.position.set((length/2) - (width/2), 0, 0)
+        fourthSide.rotation.y = Math.PI / 2;
+        tableOutside.add( fourthSide );
+
+        this.table.add(tableOutside)
+
+        this.scene.add(this.table)
     }
 
 
     render = () => {
         TWEEN.update();
-        camera.updateProjectionMatrix();
+        this.camera.updateProjectionMatrix();
 
 
         requestAnimationFrame(this.render);
